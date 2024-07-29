745366fcdd67fc87dcc8cb734a7adbbaab76cbca
andrew lee <68302962+andrew-lee-techlabs@users.noreply.github.com>
2024-07-29 07:30:54 -0500

    Update loginfo.sh

--- loginfo.sh
+++ loginfo.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
+<< comm
+IBM Copy Right 2024
+Author: Andrew Lee
+Description: Scans the s3 bucket and counts files uploaded.
+comm
 sc_store=(10002 10024 10032)
 for store in ${sc_store[@]}
 do
     cd /home/andrew/work/temp/techlabs/slim_chick/store.$store
     s=$(find .  -type f -name cmsservice.log | wc -l)
-    day=$(date | awk '{print $2}')
-    y="$((day-1))"
-    flac=$(aws s3 ls s3://aidt-chat-logs-sc/aidt_logs/v1/prod/2024-07-$y/store.$store   --recursive | grep -iEc session.flac)
-    mqtt=$(aws s3 ls s3://aidt-chat-logs-sc/aidt_logs/v1/prod/2024-07-$y/store.$store   --recursive | grep -iEc mqtt*)
+    yesterday=$(date -v-1d +%F)
+    flac=$(aws s3 ls s3://aidt-chat-logs-sc/aidt_logs/v1/prod/$yesterday/store.$store   --recursive | grep -iEc session.flac)
+    mqtt=$(aws s3 ls s3://aidt-chat-logs-sc/aidt_logs/v1/prod/$yesterday/store.$store   --recursive | grep -iEc mqtt*)
     printf "Store %s:\n" "$store"
     printf "\tCMS %d\n" "$s"
     printf "\tSession flac %d\n" "$flac"
